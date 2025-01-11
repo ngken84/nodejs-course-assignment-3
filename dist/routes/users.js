32d0c7c2c00e371603ea61761553d32ff9fc7805
Ken Ng <ng.ken84@gmail.com>
2025-01-10 20:13:17 -0800

    Add list of users

--- dist/routes/users.js
+++ dist/routes/users.js
@@ -3,14 +3,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.users = void 0;
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
+exports.users = [];
 router.get('/users', (req, res, next) => {
     res.render('users', {
-        pageTitle: 'Users'
+        pageTitle: 'Users',
+        users: exports.users
     });
 });
+router.post('/create', (req, res, next) => {
+    const { user } = req.body;
+    exports.users.push(user);
+    res.redirect('./');
+});
 router.get('/', (req, res, next) => {
+    console.log(exports.users);
     res.render('home', {
         pageTitle: 'Home'
     });
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,13 +2,23 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
+export const users: string[] = [];
+
 router.get('/users', (req: Request, res: Response, next: () => void) => {
     res.render('users', {
-        pageTitle: 'Users'
+        pageTitle: 'Users',
+        users
     });
 });
 
+router.post('/create', (req: Request, res: Response, next: () => void) => {
+    const { user } = req.body as { user: string };
+    users.push(user);
+    res.redirect('./');
+});
+
 router.get('/', (req: Request, res: Response, next: () => void) => {
+    console.log(users);
     res.render('home', {
         pageTitle: 'Home'
     });
